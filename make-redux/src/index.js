aa1921923bf8ee999d050a5d677810795d29f2c6
enhezzz <13450737557@163.com>
2018-06-18 10:57:24 +0800

    封装全局oldState到store中

--- make-redux/src/index.js
+++ make-redux/src/index.js
@@ -3,17 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-let appState = {
-  title: {
-    text: 'React.js 小书',
-    color: 'red',
-  },
-  content: {
-    text: 'React.js 小书内容',
-    color: 'blue'
-  }
-}
+// let appState = {
+//   title: {
+//     text: 'React.js 小书',
+//     color: 'red',
+//   },
+//   content: {
+//     text: 'React.js 小书内容',
+//     color: 'blue'
+//   }
+// }
 function stateChanger(state, action) {
+  if (!state) {
+    return {
+      title: {
+        text: 'React.js 小书',
+        color: 'red',
+      },
+      content: {
+        text: 'React.js 小书内容',
+        color: 'blue'
+      }
+    }
+  }
   switch (action.type) {
     case 'UPDATE_TITLE_TEXT':
       return {
@@ -35,19 +47,37 @@ function stateChanger(state, action) {
       return state
   }
 }
-function createStore(state, stateChanger) {
+function createStore(reducer) {
+  let state = null,
+    oldState = {                //把旧有状态放到store里保存
+      title: {
+        text: 'React.js 小书',
+        color: 'red',
+      },
+      content: {
+        text: 'React.js 小书内容',
+        color: 'blue'
+      }
+    };
   const getState = () => state;
   let observers = [];
   const subscribe = (observer) => {      //订阅某个观察者
     observers.push(observer);
   }
   const dispatch = (action) => {
-    state = stateChanger(state, action);
+    state = reducer(state, action);
     observers.forEach(observer => {    //改变状态时对观察者做出响应
       observer();
     })
   }
-  return { getState, dispatch, subscribe }
+  function getOldState() {            //获取旧有状态
+    return oldState;
+  }
+  function updateOldState(state) {    //更新旧有状态
+    oldState = state
+  }
+  dispatch({});
+  return { getState, dispatch, subscribe, getOldState, updateOldState }
 }
 //   function dispatch (action) {
 //     switch (action.type) {
@@ -70,6 +100,7 @@ function renderApp(newAppState, oldAppState = {}) {
 }
 
 function renderTitle(newTitle, oldTitle = {}) {
+  if (newTitle === oldTitle) return // 数据没有变化就不渲染了
   console.log('renderTitle')
   const titleDOM = document.getElementById('title')
   titleDOM.innerHTML = newTitle.text
@@ -84,12 +115,17 @@ function renderContent(newContent, oldContent = {}) {
   contentDOM.style.color = newContent.color
 }
 
-const store = createStore(appState, stateChanger);
-let oldState = store.getState() // 缓存旧的 state
+const store = createStore(stateChanger);
+// let oldState = store.getState() // 缓存旧的 state
 store.subscribe(() => {
-  const newState = store.getState() // 数据可能变化，获取新的 state
-  renderApp(newState, oldState) // 把新旧的 state 传进去渲染
-  oldState = newState // 渲染完以后，新的 newState 变成了旧的 oldState，等待下一次数据变化重新渲染
+  const oldState = store.getOldState();
+  const newState = store.getState(); // 数据可能变化，获取新的 state
+  (function (newState) {             //一个只接受新state的自执行函数
+    renderApp(newState, oldState)
+  })(newState)
+  // 把新旧的 state 传进去渲染
+  // oldState = newState // 渲染完以后，新的 newState 变成了旧的 oldState，等待下一次数据变化重新渲染
+  store.updateOldState(newState)
 })
 renderApp(store.getState()) // 首次渲染页面
 store.dispatch({ type: 'UPDATE_TITLE_TEXT', text: '《React.js 小书》' }) // 修改标题文本
