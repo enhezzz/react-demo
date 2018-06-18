cadabf3bf066691e79387fb3d0a13f7fe023c494
enhezzz <13450737557@163.com>
2018-06-18 09:29:24 +0800

    优化渲染（只对变化的状态进行render）

--- make-redux/src/index.js
+++ make-redux/src/index.js
@@ -4,41 +4,51 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 let appState = {
-    title: {
-      text: 'React.js 小书',
-      color: 'red',
-    },
-    content: {
-      text: 'React.js 小书内容',
-      color: 'blue'
-    }
+  title: {
+    text: 'React.js 小书',
+    color: 'red',
+  },
+  content: {
+    text: 'React.js 小书内容',
+    color: 'blue'
   }
-  function stateChanger (state, action) {
-    switch (action.type) {
-      case 'UPDATE_TITLE_TEXT':
-        state.title.text = action.text
-        break
-      case 'UPDATE_TITLE_COLOR':
-        state.title.color = action.color
-        break
-      default:
-        break
-    }
+}
+function stateChanger(state, action) {
+  switch (action.type) {
+    case 'UPDATE_TITLE_TEXT':
+      return {
+        ...state,
+        title: {
+          ...state.title,
+          text: action.text
+        }
+      }
+    case 'UPDATE_TITLE_COLOR':
+      return {
+        ...state,
+        title: {
+          ...state.title,
+          color: action.color
+        }
+      }
+    default:
+      return state
   }
-  function createStore (state, stateChanger) {
-    const getState = () => state;
-    let observers = [];
-    const subscribe = (observer)=>{      //订阅某个观察者
-        observers.push(observer);
-    }
-    const dispatch = (action) =>{
-        stateChanger(state, action);
-        observers.forEach(observer=>{    //改变状态时对观察者做出响应
-            observer();
-        })
-    } 
-    return { getState, dispatch,subscribe }
+}
+function createStore(state, stateChanger) {
+  const getState = () => state;
+  let observers = [];
+  const subscribe = (observer) => {      //订阅某个观察者
+    observers.push(observer);
   }
+  const dispatch = (action) => {
+    state = stateChanger(state, action);
+    observers.forEach(observer => {    //改变状态时对观察者做出响应
+      observer();
+    })
+  }
+  return { getState, dispatch, subscribe }
+}
 //   function dispatch (action) {
 //     switch (action.type) {
 //       case 'UPDATE_TITLE_TEXT':
@@ -51,26 +61,36 @@ let appState = {
 //         break
 //     }
 //   }
-  
-  function renderApp (appState) {
-    renderTitle(appState.title)
-    renderContent(appState.content)
-  }
-  
-  function renderTitle (title) {
-    const titleDOM = document.getElementById('title')
-    titleDOM.innerHTML = title.text
-    titleDOM.style.color = title.color
-  }
-  
-  function renderContent (content) {
-    const contentDOM = document.getElementById('content')
-    contentDOM.innerHTML = content.text
-    contentDOM.style.color = content.color
-  }
-  
-  const store = createStore(appState, stateChanger)
-store.subscribe(renderApp.bind(this,store.getState()))
+
+function renderApp(newAppState, oldAppState = {}) {
+  if (newAppState === oldAppState) return // 数据没有变化就不渲染了
+  console.log('renderApp')
+  renderTitle(newAppState.title, oldAppState.title)
+  renderContent(newAppState.content, oldAppState.content)
+}
+
+function renderTitle(newTitle, oldTitle = {}) {
+  console.log('renderTitle')
+  const titleDOM = document.getElementById('title')
+  titleDOM.innerHTML = newTitle.text
+  titleDOM.style.color = newTitle.color
+}
+
+function renderContent(newContent, oldContent = {}) {
+  if (newContent === oldContent) return // 数据没有变化就不渲染了
+  console.log('renderContent')
+  const contentDOM = document.getElementById('content')
+  contentDOM.innerHTML = newContent.text
+  contentDOM.style.color = newContent.color
+}
+
+const store = createStore(appState, stateChanger);
+let oldState = store.getState() // 缓存旧的 state
+store.subscribe(() => {
+  const newState = store.getState() // 数据可能变化，获取新的 state
+  renderApp(newState, oldState) // 把新旧的 state 传进去渲染
+  oldState = newState // 渲染完以后，新的 newState 变成了旧的 oldState，等待下一次数据变化重新渲染
+})
 renderApp(store.getState()) // 首次渲染页面
 store.dispatch({ type: 'UPDATE_TITLE_TEXT', text: '《React.js 小书》' }) // 修改标题文本
 store.dispatch({ type: 'UPDATE_TITLE_COLOR', color: 'blue' }) // 修改标题颜色
